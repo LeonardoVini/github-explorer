fb9e7d78f086e157c7a53b3c3be8d4c01b9f2255
Leonardo Oliveira <lleonardo@equinix.com>
2020-05-20 18:21:24 -0300

    Updates

---
+++ precache-manifest.54608b11a228140c400a17fa76777475.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7174b43b6d3beaf85a17c99c65ba1f8",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "473a7bb4e62dd60a9ec3",
+    "url": "/github-explorer/static/js/2.4fe3d710.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.4fe3d710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b52664a0110beb4c3096",
+    "url": "/github-explorer/static/js/main.c1cb5134.chunk.js"
+  },
+  {
+    "revision": "e2f0772bece6068ae40c",
+    "url": "/github-explorer/static/js/runtime-main.906dbdf0.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
